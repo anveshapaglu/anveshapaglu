debcc780e2013bfc855919ef853b6364fc58efd8
anveshapaglu <studiesketan@gmail.com>
2025-07-31 07:53:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const dynamicText = document.querySelector("h1 span");
-const words = [ "Shakarpaaree", "Meri Pari"];
+const words = [ "Sohne Sohne Baby", "Mere Little Preety Baby"];
 
 // Variables to track the position and deletion status of the word
 let wordIndex = 0;
